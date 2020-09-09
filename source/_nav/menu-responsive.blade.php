cbb6dc2f84cf74d81d7bb401024c8055f2084f6c
Sarpong97-Tom <53011231+Sarpong97-Tom@users.noreply.github.com>
2020-09-09 10:00:01 +0000

    added huberger menu (#9)

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,25 +1,32 @@
 <nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
-    <ul class="my-0">
+    <ul id="reponsive-menu" class="my-0">
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Blog"
-                href="/blog"
+                href="/"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/blog') ? 'active text-blue' : '' }}"
-            >Blog</a>
+            >Home</a>
         </li>
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} About"
-                href="/about"
+                href="/services"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/about') ? 'active text-blue' : '' }}"
-            >About</a>
+            >Services</a>
+        </li>
+        <li class="pl-4">
+            <a
+                title="{{ $page->siteName }} Contact"
+                href="/about"
+                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/contact') ? 'active text-blue' : '' }}"
+            >About us</a>
         </li>
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Contact"
                 href="/contact"
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/contact') ? 'active text-blue' : '' }}"
-            >Contact</a>
+            >Contact us</a>
         </li>
     </ul>
 </nav>
