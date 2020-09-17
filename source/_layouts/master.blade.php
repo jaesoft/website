4ddc198c7c6b3fe9bea19d82666fa808506ff6fe
tom <sarpongthomas05@gmail.com>
2020-09-17 14:23:01 +0000

    fix

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -50,7 +50,7 @@
 			</div>
 		</footer>
 		<!-- /footer -->
-        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>z
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
         @stack('scripts')
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
